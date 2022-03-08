92fda7bd1ce7024e103d3f7e20b71be1c8493355
irahul <rindhane@users.noreply.github.com>
2022-03-08 12:51:09 +0530

    refactor: build script for deployable client

--- dotnetService/TestProxyClient/build_app.ps1
+++ dotnetService/TestProxyClient/build_app.ps1
@@ -5,7 +5,7 @@ rm config_temp.json;#json files are being automatically pulled by the publish st
 mkdir $appFolder ;
 dotnet publish -c Release -r win-x64 --self-contained true -o $buildFolder ;
 mv $buildFolder\* $appFolder ;
-cp "RECORD.db" , "sqlite3.exe" $appFolder ; #configuration file for proxy client
+cp "RECORD.db" , "sqlite3.exe", "dbData.txt" $appFolder ; #configuration file for proxy client
 Compress-Archive -Path $appFolder ,
                         check_command_PS2.ps1,
                         startupScript.ps1 `
